29f860d13e282194266f7215d39c22cc0860bb2b
Sushant Nandwani <nandwani160698@gmail.com>
2025-11-04 21:51:42 +0100

    fix: remove unnecessary environment variables from build step and clean up base URL handling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-const repo = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
-const isGithubPages = process.env.GITHUB_ACTIONS === "true" && repo && !repo.endsWith(".github.io");
-const base = isGithubPages ? `/${repo}/` : "/";
-
 // https://astro.build/config
 export default defineConfig({
     output: "static",
-    base,
 });
