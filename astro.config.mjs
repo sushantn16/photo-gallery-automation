221790fb2086a28c33ceddc242e121230041a859
Sushant Nandwani <nandwani160698@gmail.com>
2025-11-04 20:01:50 +0100

    feat: github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+const repo = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
+const isGithubPages = process.env.GITHUB_ACTIONS === "true" && repo && !repo.endsWith(".github.io");
+const base = isGithubPages ? "/Stilllight/" : "/";
+
 // https://astro.build/config
 export default defineConfig({
     output: "static",
+    base,
 });
